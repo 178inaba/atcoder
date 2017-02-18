0d26cd36d0230e043eecee4175f203292fc979ef
178inaba <178inaba@users.noreply.github.com>
2017-02-19 02:26:24 +0900

    Fix "abc055_d".

--- abc055/abc055_d/main.go
+++ abc055/abc055_d/main.go
@@ -12,113 +12,51 @@ func main() {
 	N := nextInt()
 	s := nextString()
 
-	fw := make([]string, N)
-	fs := make([]string, N)
-	fw[0], fw[1] = "", "W"
-	fs[0], fs[1] = "", "S"
-	okFW, okFS := true, true
-	for i := 1; i < N; i++ {
-		said := string(s[i-1])
-		// First is wolf.
-		switch said {
-		case "o":
-			if i+1 == N {
-				if fw[i-1] == "S" {
-					if fw[0] != "W" {
-						okFW = false
+	s += string(s[0])
+	starts := [][]string{{"S", "S"}, {"S", "W"}, {"W", "S"}, {"W", "W"}}
+	for _, start := range starts {
+		animals := make([]string, N+2)
+		animals[0] = start[0]
+		animals[1] = start[1]
+		for i := 1; i <= N; i++ {
+			if animals[i] == "S" {
+				if s[i] == 'o' {
+					if animals[i-1] == "S" {
+						animals[i+1] = "S"
+					} else {
+						animals[i+1] = "W"
 					}
 				} else {
-					if fw[0] != "S" {
-						okFW = false
-					}
-				}
-			} else {
-				if fw[i-1] == "" {
-					fw[i-1] = "S"
-					fw[i+1] = "W"
-				} else if fw[i-1] == "S" {
-					fw[i+1] = "W"
-				} else {
-					fw[i+1] = "S"
-				}
-			}
-		case "x":
-			if i+1 == N {
-				if fw[i-1] == "S" {
-					if fw[0] != "S" {
-						okFW = false
-					}
-				} else {
-					if fw[0] != "W" {
-						okFW = false
+					if animals[i-1] == "S" {
+						animals[i+1] = "W"
+					} else {
+						animals[i+1] = "S"
 					}
 				}
 			} else {
-				if fw[i-1] == "" {
-					fw[i-1] = "S"
-					fw[i+1] = "S"
-				} else if fw[i-1] == "S" {
-					fw[i+1] = "S"
+				if s[i] == 'o' {
+					if animals[i-1] == "S" {
+						animals[i+1] = "W"
+					} else {
+						animals[i+1] = "S"
+					}
 				} else {
-					fw[i+1] = "W"
+					if animals[i-1] == "S" {
+						animals[i+1] = "S"
+					} else {
+						animals[i+1] = "W"
+					}
 				}
 			}
 		}
 
-		// First is sheep.
-		switch said {
-		case "o":
-			if i+1 == N {
-				if fs[i-1] == "S" {
-					if fs[0] != "S" {
-						okFS = false
-					}
-				} else {
-					if fs[0] != "W" {
-						okFS = false
-					}
-				}
-			} else {
-				if fs[i-1] == "" {
-					fs[i-1] = "S"
-					fs[i+1] = "S"
-				} else if fs[i-1] == "S" {
-					fs[i+1] = "S"
-				} else {
-					fs[i+1] = "W"
-				}
-			}
-		case "x":
-			if i+1 == N {
-				if fs[i-1] == "S" {
-					if fs[0] != "W" {
-						okFS = false
-					}
-				} else {
-					if fs[0] != "S" {
-						okFS = false
-					}
-				}
-			} else {
-				if fs[i-1] == "" {
-					fs[i-1] = "S"
-					fs[i+1] = "W"
-				} else if fs[i-1] == "S" {
-					fs[i+1] = "W"
-				} else {
-					fs[i+1] = "S"
-				}
-			}
+		if animals[0] == animals[N] && animals[1] == animals[N+1] {
+			fmt.Println(strings.Join(animals[:N], ""))
+			return
 		}
 	}
 
-	if okFW {
-		fmt.Println(strings.Join(fw, ""))
-	} else if okFS {
-		fmt.Println(strings.Join(fs, ""))
-	} else {
-		fmt.Println(-1)
-	}
+	fmt.Println(-1)
 }
 
 // Input. ----------
