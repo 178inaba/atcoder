e3da181ce9184f35a9f8337ccb734454881c1815
178inaba <178inaba@users.noreply.github.com>
2017-02-18 20:25:21 +0900

    Prepare "abc055".

---
+++ abc055/abc055_a/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+}
+
+// Input. ----------
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+// ---------- Input.
