cd653c6a013702d532dbff6607f2e6310396a7b5
178inaba <178inaba@users.noreply.github.com>
2017-03-05 22:25:36 +0900

    WIP yahoo_procon2017_qual_c.

--- yahoo-procon2017-qual/yahoo_procon2017_qual_c/main.go
+++ yahoo-procon2017-qual/yahoo_procon2017_qual_c/main.go
@@ -2,13 +2,70 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 func main() {
+	N := nextInt()
+	K := nextInt()
 
+	A := make([]int, 0, K)
+	for i := 0; i < K; i++ {
+		A = append(A, nextInt())
+	}
+
+	S := make([]string, 1, N)
+	for i := 0; i < N; i++ {
+		S = append(S, nextString())
+	}
+
+	if N == K {
+		fmt.Println()
+		return
+	}
+
+	targets := make([]string, 0, K)
+	for _, a := range A {
+		targets = append(targets, S[a])
+	}
+
+	sort.Strings(targets)
+	base := targets[0]
+	charCnt := len(base)
+	for i := 0; i < charCnt; i++ {
+		result := true
+		for j := 1; j < len(targets); j++ {
+			if !strings.HasPrefix(targets[j], base) {
+				result = false
+				break
+			}
+		}
+
+		if result == true {
+			break
+		}
+
+		base = base[:len(base)-1]
+	}
+
+	if len(base) == 0 {
+		fmt.Println(-1)
+		return
+	}
+
+	for i := 1; i <= N; i++ {
+		if !strings.HasPrefix(S[i], base) {
+			fmt.Println(base)
+			return
+		}
+	}
+
+	fmt.Println(-1)
 }
 
 // Input. ----------
