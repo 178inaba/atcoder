67b441bb5296196932b176766d433d156ff82c4b
178inaba <178inaba@users.noreply.github.com>
2017-05-13 21:04:13 +0900

    Accepted abc061_a

---
+++ abc061/abc061_a/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	A := nextInt()
+	B := nextInt()
+	C := nextInt()
+
+	if C >= A && C <= B {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+
+// Input. ----------
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+func nextString() string {
+	sc.Scan()
+	if err := sc.Err(); err != nil {
+		panic(err)
+	}
+
+	return sc.Text()
+}
+
+// ---------- Input.
+
+// Util. ----------
+
+func min(x, y int) int {
+	return int(math.Min(float64(x), float64(y)))
+}
+
+func max(x, y int) int {
+	return int(math.Max(float64(x), float64(y)))
+}
+
+// ---------- Util.
