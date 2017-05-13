2502f522b6020896425666b1cc1e763602fc74b4
178inaba <178inaba@users.noreply.github.com>
2017-05-14 00:02:52 +0900

    Fix abc061_d

--- abc061/abc061_d/main.go
+++ abc061/abc061_d/main.go
@@ -2,13 +2,74 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"math"
 	"os"
 	"strconv"
 )
 
 func main() {
+	N := nextInt()
+	M := nextInt()
+	var gs graphs
+	for i := 0; i < M; i++ {
+		gs = append(gs, graph{a: nextInt(), b: nextInt(), c: nextInt()})
+	}
 
+	var max int
+	for i, gg := range gs {
+		if gg.a == 1 {
+			gscp := make(graphs, len(gs))
+			copy(gscp, gs)
+			gscp[i].isPass = true
+			ret := gscp.calc(1, gg.b, gg.c, N)
+			if ret > max {
+				max = ret
+			}
+		}
+	}
+
+	fmt.Println(max)
+}
+
+type graph struct {
+	a, b, c int
+	isPass  bool
+}
+
+type graphs []graph
+
+func (gs *graphs) calc(a, b, c, N int) int {
+	var g graph
+	var isExist bool
+	for i, gg := range *gs {
+		if a == gg.a && b == gg.b {
+			(*gs)[i].isPass = true
+			isExist = true
+			g = gg
+			break
+		}
+	}
+
+	if !isExist {
+		return 0
+	}
+
+	var max int
+	for _, gg := range *gs {
+		if b == gg.a {
+			if gg.isPass {
+				fmt.Println("inf")
+				os.Exit(0)
+			}
+			ret := gs.calc(b, gg.b, gg.c, N)
+			if ret > max {
+				max = ret
+			}
+		}
+	}
+
+	return g.c + max
 }
 
 // Input. ----------
