17ac6f55d8b36a9e800bba98028218a422905e59
178inaba <178inaba@users.noreply.github.com>
2017-07-03 00:40:19 +0900

    Fix abc066/arc077_b

--- abc066/arc077_b/main.go
+++ abc066/arc077_b/main.go
@@ -2,13 +2,59 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"math"
 	"os"
 	"strconv"
 )
 
-func main() {
+const (
+	maxN = 200005
+	mod  = 1000000007
+)
 
+func main() {
+	n := nextInt()
+	as := make([]int, n+2)
+	var maxNum, s, pre, nex int
+	for i := 1; i <= n+1; i++ {
+		as[i] = nextInt()
+		maxNum = max(maxNum, as[i])
+	}
+	vis := make([]bool, maxNum+1)
+	inv := make([]int, maxN)
+	f := make([]int, maxN)
+	invf := make([]int, maxN)
+	for i := 1; i <= n+1; i++ {
+		if vis[as[i]] {
+			s = as[i]
+		}
+		vis[as[i]] = true
+	}
+	for i := 1; i <= n+1 && as[i] != s; i++ {
+		pre++
+	}
+	for i := n + 1; i >= 0 && as[i] != s; i-- {
+		nex++
+	}
+	f[0], invf[0] = 1, 1
+	inv[1], f[1], invf[1] = 1, 1, 1
+	for i := 2; i <= nex+pre || i <= n+1; i++ {
+		inv[i] = mod - (mod/i)*inv[mod%i]%mod
+		f[i] = f[i-1] * i % mod
+		invf[i] = invf[i-1] * inv[i] % mod
+	}
+	for i := 1; i <= n+1; i++ {
+		ans := (c(n+1, i, f, invf) - c(pre+nex, i-1, f, invf) + mod) % mod
+		fmt.Println(ans)
+	}
+}
+
+func c(n, m int, f, invf []int) int {
+	if n < m {
+		return 0
+	}
+	return f[n] * invf[m] % mod * invf[n-m] % mod
 }
 
 // Input. ----------
