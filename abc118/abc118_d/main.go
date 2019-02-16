6a3ae1aac376e2264e7af58857c7ae5849974a2b
178inaba <178inaba@users.noreply.github.com>
2019-02-16 22:42:20 +0900

    Time is up of `abc118_d`

--- abc118/abc118_d/main.go
+++ abc118/abc118_d/main.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"math"
 	"os"
 	"strconv"
 )
 
 func main() {
+	N := nextInt()
+	M := nextInt()
+	var As []int
+	for i := 0; M > i; i++ {
+		As = append(As, nextInt())
+	}
 
+	numMap := map[int]int{1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
+	priorities := []int{1, 7, 4, 5, 3, 2, 9, 6, 8}
+	var currentPriority []int
+	for _, priority := range priorities {
+		for _, A := range As {
+			if A == priority {
+				currentPriority = append(currentPriority, A)
+			}
+		}
+	}
+
+	parts := map[int]int{}
+	for N > 0 {
+		fmt.Println(N)
+		for _, cp := range currentPriority {
+			if N > numMap[cp] {
+				N -= numMap[cp]
+				parts[cp]++
+				break
+			}
+		}
+	}
+
+	fmt.Println(parts)
 }
 
 // Input. ----------
