84ebfce181f03cf38718aacbd8fa92bc49667d49
178inaba <178inaba@users.noreply.github.com>
2019-02-25 01:41:17 +0900

    AC abc119_d

--- abc119/abc119_d/main.go
+++ abc119/abc119_d/main.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func main() {
+	INF := int(math.Pow10(11))
+	A := nextInt()
+	B := nextInt()
+	Q := nextInt()
+	s := make([]int, A+2)
+	s[0], s[A+1] = -INF, INF
+	for i := 1; i <= A; i++ {
+		s[i] = nextInt()
 
+	}
+
+	t := make([]int, B+2)
+	t[0], t[B+1] = -INF, INF
+	for i := 1; i <= B; i++ {
+		t[i] = nextInt()
+	}
+
+	for i := 0; i < Q; i++ {
+		x := nextInt()
+		b, d := sort.SearchInts(s, x), sort.SearchInts(t, x)
+		res := INF
+		for _, S := range []int{s[b-1], s[b]} {
+			for _, T := range []int{t[d-1], t[d]} {
+				d1, d2 := abs(S-x)+abs(T-S), abs(T-x)+abs(S-T)
+				res = min(res, min(d1, d2))
+			}
+		}
+
+		fmt.Println(res)
+	}
 }
 
 // Input. ----------
