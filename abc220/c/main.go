aac21822357a11edaca89bd35512acff565efaaa
Masahiro Furudate <178inaba.git@gmail.com>
2021-09-26 21:44:44 +0900

    AC abc220/c

---
+++ abc220/c/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	N := nextInt()
+
+	As := make([]int, N)
+	var sumA int
+	for i := 0; i < N; i++ {
+		As[i] = nextInt()
+		sumA += As[i]
+	}
+
+	X := nextInt()
+
+	cnt := ((X / sumA) * len(As)) + len(As)
+	sum := sumA * (cnt / len(As))
+	var loopCnt int
+	for i := len(As) - 1; i > -1; i-- {
+		loopCnt++
+		sum -= As[i]
+		if sum <= X {
+			fmt.Println(cnt - (loopCnt - 1))
+			return
+		}
+	}
+}
+
+// Input. ----------
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+func nextFloat() float64 {
+	sc.Scan()
+	f64, err := strconv.ParseFloat(sc.Text(), 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return f64
+}
+
+func nextString() string {
+	sc.Scan()
+	if err := sc.Err(); err != nil {
+		panic(err)
+	}
+
+	return sc.Text()
+}
+
+// ---------- Input.
+
+// Util. ----------
+
+func abs(x int) int {
+	return int(math.Abs(float64(x)))
+}
+
+func min(x, y int) int {
+	return int(math.Min(float64(x), float64(y)))
+}
+
+func max(x, y int) int {
+	return int(math.Max(float64(x), float64(y)))
+}
+
+// ---------- Util.
